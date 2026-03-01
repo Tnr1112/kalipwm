ffbe5563187a8bcab3b3c66824fac1439e3cc822
Tnr1112 <tnr1112@hotmail.com>
2026-03-01 16:14:23 -0300

    Agrego la batería en la polybar si se está ejecutando desde una notebook

--- SCRIPTS/battery.sh
+++ SCRIPTS/battery.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-if [ "$(acpi | awk '{print $3}' | tr -d ',')" != "Discharging" ]; then
+BATTERY_PATH=$(ls -d /sys/class/power_supply/BAT* 2>/dev/null | head -n 1)
+[ -z "$BATTERY_PATH" ] && exit 0
 
-	echo -e "%{F#27FF00} %{F#e2ee6a}$(acpi | awk '{print $4}' | tr -d ',')%{u-}"
+command -v acpi >/dev/null 2>&1 || exit 0
+
+BAT_INFO=$(acpi -b | head -n 1)
+[ -z "$BAT_INFO" ] && exit 0
+
+STATE=$(echo "$BAT_INFO" | awk -F': ' '{print $2}' | awk -F', ' '{print $1}')
+PERCENT=$(echo "$BAT_INFO" | awk -F', ' '{print $2}')
+
+[ -z "$PERCENT" ] && PERCENT="$(cat "$BATTERY_PATH/capacity" 2>/dev/null)%"
+[ -z "$PERCENT" ] && exit 0
+
+if [ "$STATE" = "Discharging" ]; then
+	echo -e "%{F#FF0000} %{F#e2ee6a}${PERCENT}%{u-}"
 else
-	echo -e "%{F#FF0000} %{F#e2ee6a}$(acpi | awk '{print $4}' | tr -d ',')%{u-}"
+	echo -e "%{F#27FF00} %{F#e2ee6a}${PERCENT}%{u-}"
 fi
