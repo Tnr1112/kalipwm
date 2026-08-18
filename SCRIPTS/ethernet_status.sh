60cf580630bea07f92d416e71a334cd71d364d8f
Tnr1112 <tnr1112@hotmail.com>
2026-08-18 19:12:57 -0300

    Script de ethernet para que agarre cualquier interfaz

--- SCRIPTS/ethernet_status.sh
+++ SCRIPTS/ethernet_status.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
  
-echo "%{F#2495e7}󰈀 %{F#ffffff}$(/usr/sbin/ifconfig eth0 | grep "inet " | awk '{print $2}')%{u-}"
+echo "%{F#2495e7}󰈀 %{F#ffffff}$(/usr/sbin/ip -4 addr show | awk '/inet / && !/127.0.0.1/ {print $2}' | cut -d/ -f1 | head -n 1)%{u-}"
